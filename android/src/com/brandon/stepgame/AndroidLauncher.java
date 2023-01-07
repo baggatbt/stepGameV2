30f6f2e5b4c09de9c1f2ae683b3c672fc0fb6dac
Brandon <baggatbt@gmail.com>
2023-01-07 12:32:29 -0500

    Initial project setup

---
+++ android/src/com/brandon/stepgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brandon.stepgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brandon.stepgame.stepGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new stepGame(), config);
+	}
+}
---
+++ desktop/src/com/brandon/stepgame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.brandon.stepgame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.brandon.stepgame.stepGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("stepApp");
+		new Lwjgl3Application(new stepGame(), config);
+	}
+}
